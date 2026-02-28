44e46e4a4e5dc13fa7df77f9773c943f234cbb60
ShreyashSriv <rishitsrivastava033@gmail.com>
2026-03-01 01:17:03 +0530

    heehe

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ var b = document.querySelector("h2")
 b.innerHTML = "shreyash srivastava"
 
 var c = document.querySelector("h3")
-b.innerHTML = "kaiseeeeee ssrgrgjydffdfvdfbddfkffvunooooooooooo"
+b.innerHTML = "kaiseeeeee ssrgrgjyjhgvdffdfvdfbddfkffvunooooooooooo"
