e46eb40853df9b420efd2541912f0c1dad766255
ShreyashSriv <rishitsrivastava033@gmail.com>
2026-03-13 15:07:25 +0530

    hlw

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ var a = document.querySelector("h1")
 a.innerHTML = "hlw"
 
 var b = document.querySelector("h2")
+
 b.innerHTML = "shreyash srivastava"
 
 var c = document.querySelector("h3")
-b.innerHTML = "kaiseeehbunjomfnbbbjjjjfhfnnkoooooooo"
+
+b.innerHTML = "kooooo reeeeee"
